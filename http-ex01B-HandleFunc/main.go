f5af0708ff28a61d1951eb296047a946875187d5
Tong Sun <suntong@cpan.org>
2019-05-04 10:28:16 -0400

    - [+] add Image() HandleFunc

--- http-ex01B-HandleFunc/main.go
+++ http-ex01B-HandleFunc/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"html"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func test0(w http.ResponseWriter, r *http.Request) {
 	// Handles top-level page.
-	fmt.Fprintf(w, "You are on the home page")
+	fmt.Fprintf(w, `You are on the home page
+
+Here are the sample urls that you can visit:
+
+- /about
+- /images
+- and /image
+
+
+`)
 }
 
 func test1(w http.ResponseWriter, r *http.Request) {
@@ -24,11 +36,27 @@ func test2(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Image page")
 }
 
+func Image(w http.ResponseWriter, r *http.Request) {
+	// Open a JPG file.
+	f, _ := os.Open("vlc-16x16.png")
+
+	// Read the entire JPG file into memory.
+	reader := bufio.NewReader(f)
+	content, _ := ioutil.ReadAll(reader)
+
+	// Set the Content Type header.
+	w.Header().Set("Content-Type", "image/png")
+
+	// Write the image to the response.
+	w.Write(content)
+}
+
 func main() {
 	// Add handle funcs for 3 pages.
 	http.HandleFunc("/", test0)
 	http.HandleFunc("/about", test1)
 	http.HandleFunc("/images", test2)
+	http.HandleFunc("/image", Image)
 
 	// Run the web server.
 	log.Fatal(http.ListenAndServe(":8080", nil))
